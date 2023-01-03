117e0735f4cbd478037df04a756caba063d39dc7
Karim Meiborg <Karim.Meiborg@ruv.de>
2023-01-03 11:40:43 +0100

    fix: repair broken dependency to christmas-tree

--- versions.tf
+++ versions.tf
@@ -2,6 +2,7 @@ terraform {
   required_version = ">=0.13.0"
   required_providers {
     christmas-tree = {
+      source = "cappyzawa/christmas-tree"
       version = ">= 0.3.0"
     }
     random = {
